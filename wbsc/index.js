17f72c8309c1af8e2242b6ffeae3eccfa9d25afb
ootomonaiso <ootomonaiso.tubusiki@gmail.com>
2025-02-02 11:40:09 +0900

    fix: ナビゲーションにアイコンを追加し、メインビジュアルのテキストを更新

--- wbsc/index.js
+++ wbsc/index.js
@@ -1,4 +1,4 @@
 function toggleMenu() {
-    const nav = document.querySelector('.global-nav'); // ← 正しいクラス名を指定する
+    const nav = document.querySelector('.global-nav'); // ← 正しいクラス名指定しろしろしろしろ
     nav.classList.toggle('open');
 }
